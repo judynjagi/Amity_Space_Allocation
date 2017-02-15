b1ce10408872f5b8c3ded8242c7c43f67679a26b
Judy Njangi <judy.njagi@andela.com>
2017-02-15 17:40:00 +0300

    Made changes to Amity

---
+++ app/classes/rooms.py
@@ -0,0 +1,24 @@
+class Room (object):
+
+	def __init__(self, name, room_type = "None"):
+		self.name = name.upper()
+		self.room_type = room_type.upper()
+		self.occupants = []
+		max_capacity = 0
+
+	def __repr__(self):
+		return self.name + " :" + "  " + self.room_type 
+
+	def is_full(self):
+		return len(self.occupants) >= self.max_capacity;
+				
+class LivingSpace(Room):
+
+	def __init__(self, name):
+		self.max_capacity = 4
+		super(LivingSpace, self).__init__(name, "LivingSpace")
+
+class Office (Room):
+	def __init__(self, name):
+		self.max_capacity = 6
+		super(Office, self).__init__(name, "Office")
